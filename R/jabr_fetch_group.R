da6f721ba34fdbddd88e79ae0362de83b077ad55
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2020-01-13 04:40:03 +0000

    version 0.1.2

--- R/jabr_fetch_group.R
+++ R/jabr_fetch_group.R
@@ -1,10 +1,10 @@
-#' Fetch dataset
+#' Fetch group of datasets
 #'
-#' Download and parse the dataset available at Open Data Jawa Barat into R.
+#' Download and parse the dataset available at Open Data Jawa Barat into R. This function will download multiple datasets which have same \code{group_id}.
 #'
-#' @param id The character id of dataset. It accepts single id or multiple ids. Non-matched id(s) will be ignored.
+#' @param group_id The group id of dataset. It only accepts single group id.
 #' @param keep_title Whether to keep the dataset title in result. If the result is a table, the title is saved in "title" column. If the result is a list, the title is saved as list name.
-#' @param as Applicable for multiple \code{id}. If \code{as = "table"}, the fetched datasets will be saved in "dataset" column with list-column type where you can run \code{tidyr::unnest()} afterwards. Otherwise the fetched datasets will be saved as list.
+#' @param as If \code{as = "table"}, the fetched datasets will be saved in "dataset" column with list-column type where you can run \code{tidyr::unnest()} afterwards. Otherwise the fetched datasets will be saved as list.
 #'
 #' @importFrom rlang arg_match .data
 #' @importFrom rappdirs user_cache_dir
@@ -23,41 +23,38 @@
 #' (x <- jabr_list_dataset())
 #'
 #'
-#' # for example, we want to fetch data about number of application available in west java.
-#' # The id of this data is "3a51a670".
-#' jabr_fetch("3a51a670")
+#' # for example, we want to fetch data about gini ratio in west java since 2011 to 2017.
+#' # The group_id of this data is "78358b54".
+#' jabr_fetch_group("78358b54") %>%
+#'   tidyr::unnest(cols = c(dataset))
 #' }
 #'
 #' @export
-jabr_fetch <- function(id, keep_title = TRUE, as = "table") {
+jabr_fetch_group <- function(group_id, keep_title = TRUE, as = "table") {
   as <- arg_match(as, values = c("table", "list"))
 
-  if (!is.character(id) || nchar(id) != 8L) {
-    stop("`id` must be 8 digits character", call. = FALSE)
+  if (length(group_id) > 1) {
+    stop("Please supply one `group_id` only", call. = FALSE)
+  }
+
+  if (!is.character(group_id) || nchar(group_id) != 8L) {
+    stop("`group_id` must be 8 digits character", call. = FALSE)
   }
 
   jabr_cache_dir <- user_cache_dir("jabr")
 
   load(file = file.path(jabr_cache_dir, "jabr_data.rda"))
 
-  id_ok <- keep(id, ~ is.element(.x, jabr_data$id))
-  id_not_ok <- keep(id, ~ !is.element(.x, jabr_data$id))
+  id_ok <- keep(group_id, ~ is.element(.x, jabr_data$group_id))
+  id_not_ok <- keep(group_id, ~ !is.element(.x, jabr_data$group_id))
 
-  if (length(id_not_ok) == length(id)) {
-    stop("Can't locate the supplied id(s), please re-check it.", call. = FALSE)
-  }
-
-  if (length(id_not_ok) > 1L) {
-    warning(paste("Can't locate these id(s):", paste0(id_not_ok, collapse = ", "), ". They will be ignored."))
-  }
-
-  if (length(id_not_ok) == 1L) {
-    warning(paste0("Can't locate ", id_not_ok, ". It will be ignored."))
+  if (length(id_not_ok) == length(group_id)) {
+    stop("Can't locate the supplied `group_id`, please re-check it.", call. = FALSE)
   }
 
   res <-
     jabr_data %>%
-    dplyr::filter(.data$id %in% id_ok) %>%
+    dplyr::filter(.data$group_id %in% id_ok) %>%
     mutate(dataset = map(url, ckan_fetch)) %>%
     select(
       -.data$id,
